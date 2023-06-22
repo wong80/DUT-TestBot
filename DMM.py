65145ea8cde5e1fb9160898400e564062f2bfae2
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2023-06-22 15:23:04 +0800

    Added Model N6701C

--- DMM.py
+++ DMM.py
@@ -273,7 +273,7 @@ class EDU34450A(object):
 
 
 class plotGraph:
-    def plotting(list):
+    def plotting(self, list):
         plt.plot(list)
         # naming the x axis
         plt.xlabel("x - axis")
@@ -285,7 +285,100 @@ class plotGraph:
         plt.show()
 
 
-A = EDU34450A("USB0::0x2A8D::0x8E01::CN60440004::0::INSTR")
-A.config("Primary", "Frequency")
-A.Sense("Primary", "Voltage", "AC", "RES FAST")
-A.QSense("Primary", "Voltage", "Range")
+class N6701C(EDU34450A):
+    def __init__(self, VISA_ADDRESS):
+        self.VISA_ADDRESS = VISA_ADDRESS
+        # ResourceManager Setup
+        rm = pyvisa.ResourceManager()
+
+        # Visa Address is found under Keysight Connection Expert
+        self.dmm = rm.open_resource(VISA_ADDRESS)
+        self.dmm.baud_rate = 9600
+        # '*IDN?' is standard GPIB Message for "what are you?"
+        self.dmm.timeout = 1000
+        print(self.dmm.query("*IDN?"))
+
+        # Resets the instrument configuration and synchronizes it before each R/W
+        self.dmm.write("*rst")
+        self.dmm.query("*opc?")
+
+    def measure(self, DURATION, period, list, CHANNEL_NUMBER, *args):
+        self.duration = DURATION
+        self.list = list
+        self.period = period
+
+        if len(args) == 1:
+            local_str = ""
+
+            for item in args:
+                local_str = local_str + item
+
+            cmd = "MEAS:" + local_str + "?" + " (@" + str(CHANNEL_NUMBER) + ")"
+
+        elif len(args) == 2:
+            str1 = ""
+            str2 = ""
+            for item in args[0]:
+                str1 = str1 + item
+            for item in args[1]:
+                str2 = str2 + item
+
+            cmd = "MEAS:" + str1 + ":" + str2 + "?" + " (@" + str(CHANNEL_NUMBER) + ")"
+
+        elif len(args) == 3:
+            str1 = ""
+            str2 = ""
+            str3 = ""
+            for item in args[0]:
+                str1 = str1 + item
+            for item in args[1]:
+                str2 = str2 + item
+            for item in args[2]:
+                str3 = str3 + item
+
+            cmd = (
+                "MEAS:"
+                + str1
+                + ":"
+                + str2
+                + ":"
+                + str3
+                + "?"
+                + " (@"
+                + str(CHANNEL_NUMBER)
+                + ")"
+            )
+
+        for i in range(int(self.duration)):
+            self.dmm.timeout = 10000
+            # print(cmd)
+            list.append(float(self.dmm.query(cmd)))
+            time.sleep(self.period)
+
+        print(list)
+
+    def Output(self, state, CHANNEL_NUMBER):
+        self.channel_number = CHANNEL_NUMBER
+        self.dmm.write("OUTPUT " + state + "," + " (@" + CHANNEL_NUMBER + ")")
+
+    def setCurrent(self, value, CHANNEL_NUMBER):
+        self.dmm.write("CURR " + value + "," + " (@" + CHANNEL_NUMBER + ")")
+
+    def Measure(self, param, CHANNEL_NUMBER):
+        print(self.dmm.query("MEAS:" + param + "?" + " (@" + CHANNEL_NUMBER + ")"))
+
+    def testFunc(self):
+        print("test")
+
+
+# A = EDU34450A("USB0::0x2A8D::0x8E01::CN60440004::0::INSTR")
+# A.config("Primary", "Frequency")
+# A.Sense("Primary", "Voltage", "AC", "RES FAST")
+# A.QSense("Primary", "Voltage", "Range")
+
+B = N6701C("USB0::0x2A8D::0x0102::MY56000223::0::INSTR")
+B.Output("ON", "3")
+B.setCurrent("1", "3")
+B.measure(50, 0.1, list, 3, "Voltage")
+C = plotGraph()
+C.plotting(list)
