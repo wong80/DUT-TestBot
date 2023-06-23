8fdd74c709a21f7d2beaad904165a928c0f82693
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2023-06-23 09:48:07 +0800

    Added new Functions

--- PSU.py
+++ PSU.py
@@ -1,6 +1,9 @@
 import pyvisa
 import time
 import matplotlib.pyplot as plt
+import array
+
+dataList = []
 
 
 class E36731A(object):
@@ -28,7 +31,12 @@ class E36731A(object):
         self.setCurrent = CURRENT_SET
 
         self.dmm.write(
-            "APPL " + self.Channel + "," + self.setVoltage + "," + self.setCurrent
+            "APPL "
+            + self.Channel
+            + ","
+            + str(self.setVoltage)
+            + ","
+            + str(self.setCurrent)
         )
 
     def Emulate(self, Emul):
@@ -38,9 +46,31 @@ class E36731A(object):
     def Output(self, state):
         self.dmm.write("OUTPUT " + state)
 
+    def testFunc(self, minVoltage, maxVoltage, Current, Channel, step_size, dataList):
+        self.dataList = dataList
+        self.Channel = Channel
+        self.minVoltage = minVoltage
+        self.maxVoltage = maxVoltage
+        self.Current = Current
+        self.step_size = step_size
+
+        k = minVoltage
+        i = 0
+        while k <= maxVoltage:
+            self.dmm.write(
+                "APPL " + self.Channel + "," + str(k) + "," + str(self.Current)
+            )
+            print("Voltage:  Current:    ")
+            print(self.dmm.query("APPL?"))
+            self.dataList.insert(i, [k, self.Current])
+            time.sleep(self.step_size)
+
+            k += step_size
+            i += 1
+        print(self.dataList)
+
 
 A = E36731A("USB0::0x2A8D::0x5C02::MY62100050::0::INSTR")
-A.apply("CH1", "7", "2")
 A.Output("ON")
-time.sleep(5)
+A.testFunc(5, 10, 2, "CH1", 0.5, dataList)
 A.Output("OFF")
