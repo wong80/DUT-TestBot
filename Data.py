88f8990570bc4ed396dbf60e00487036e6cee7a5
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2023-06-26 14:58:58 +0800

    Revert "Revert "Updated Data.py and added data.csv""
    This reverts commit af3065c3782bed5eefe7ae288146374341c34053.

--- Data.py
+++ Data.py
@@ -1,7 +1,13 @@
 import PSU
+import DMM
 import pandas as pd
+from multiprocessing import Process
 
 
 class datatoCSV(object):
-    df = pd.DataFrame(PSU.dataList, columns=["Voltage", "Current"])
-    df.to_csv("data.csv")
+    def __init__(self):
+        self.df1 = pd.DataFrame(PSU.infoList, columns=["setVoltage", "Current"])
+        self.df2 = pd.DataFrame(DMM.dataList, columns=["Measured Voltage"])
+        self.df3 = pd.concat([self.df1, self.df2], axis=1)
+        self.df3.to_csv("data.csv")
+        print(self.df3.to_string)
