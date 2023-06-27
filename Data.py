635b70f51d539b47a502ab429c8ea1cd2466ebfb
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2023-06-27 09:15:55 +0800

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,13 +1,14 @@
 import PSU
 import DMM
 import pandas as pd
-from multiprocessing import Process
 
 
 class datatoCSV(object):
-    def __init__(self):
-        self.df1 = pd.DataFrame(PSU.infoList, columns=["setVoltage", "Current"])
-        self.df2 = pd.DataFrame(DMM.dataList, columns=["Measured Voltage"])
+    def __init__(self, infoList, dataList):
+        self.infoList = infoList
+        self.dataList = dataList
+        self.df1 = pd.DataFrame(self.infoList, columns=["setVoltage", "Current"])
+        self.df2 = pd.DataFrame(self.dataList, columns=["Measured Voltage"])
         self.df3 = pd.concat([self.df1, self.df2], axis=1)
         self.df3.to_csv("data.csv")
         print(self.df3.to_string)
