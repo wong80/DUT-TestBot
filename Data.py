bbe47386dfa686445e0d111c508a8153a98e7798
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2023-06-30 10:06:44 +0800

    Updated Data.py
    Has the function to determine if the plotted results have exceeded a certain error margin and visualize on a graph

--- Data.py
+++ Data.py
@@ -8,23 +8,26 @@ class datatoCSV(object):
         self.infoList = infoList
         self.dataList = dataList
 
-        self.sf1 = pd.Series(self.infoList)
-        self.sf2 = pd.Series(self.dataList)
-        self.sf3 = (self.sf1 - self.sf2) / self.sf1 * 100
-        self.sf4 = self.sf1 - self.sf2
+        self.Vset = pd.Series(self.infoList)
+        self.Vmeasured = pd.Series(self.dataList)
+        self.absolute_error = abs(self.Vset - self.Vmeasured)
+        self.percent_error = self.absolute_error / self.Vset * 100
 
-        self.df1 = self.sf1.to_frame(name="Voltage Set")
-        self.df2 = self.sf2.to_frame(name="Voltage Measured")
-        self.df3 = self.sf3.to_frame(name="Programmable Error (%)")
-        # self.df4 = self.sf4.to_frame(name="error")
-        self.df5 = pd.concat([self.df1, self.df2, self.df3], axis=1)
+        self.VsetF = self.Vset.to_frame(name="Voltage Set")
+        self.VmeasuredF = self.Vmeasured.to_frame(name="Voltage Measured")
+        self.absolute_errorF = self.absolute_error.to_frame(name="Absolute Error")
+        self.percent_errorF = self.percent_error.to_frame(name="Percentage Error (%)")
+        self.CSV1 = pd.concat(
+            [self.VsetF, self.VmeasuredF, self.absolute_errorF, self.percent_errorF],
+            axis=1,
+        )
 
-        self.df5.to_csv("data.csv")
-        # print(self.df4.to_string)
+        self.CSV1.to_csv("data.csv")
 
 
-class datatoGraph:
-    def __init__(self):
+class datatoGraph(datatoCSV):
+    def __init__(self, infoList, dataList):
+        super().__init__(infoList, dataList)
         self.data = pd.read_csv("data.csv")
 
     def plotLine(self):
@@ -35,42 +38,47 @@ class datatoGraph:
         self.param1 = param1
         self.param2 = param2
 
-        x = self.data["Voltage Set"].to_numpy()
-        y = self.data["Voltage Measured"].to_numpy()
-        a, b = np.polyfit(x, y, 1)
-        # IDEAL_M = 0.00025  # 0.25%
-        # IDEAL_C = 0.0015  # 1.5mV
-        upper_bound = x + (self.param1 * x + self.param2)
-        lower_bound = x - (self.param1 * x - self.param2)
-        self.sf6 = pd.Series(upper_bound)
-        self.sf7 = pd.Series(lower_bound)
-        self.df6 = self.sf6.to_frame(name="Upper Boundary")
-        self.df7 = self.sf7.to_frame(name="Lower Boundary")
+        a, b = np.polyfit(self.Vset, self.percent_error, 1)
+        self.error_limit = self.param1 * self.Vset + self.param2 * 100
+        self.error_margin = self.error_limit * 0.6
+        self.condition = self.error_margin < self.percent_error
 
-        self.df8 = pd.concat([self.data, self.df6, self.df7], axis=1)
+        self.error_limitF = self.error_limit.to_frame(name="Error Limit")
+        # self.error_marginF = self.error_margin.to_frame(name="Error Margin")
+        self.conditionF = self.condition.to_frame(name="Condition ?")
 
-        self.df8.to_csv("graph.csv")
+        self.CSV2 = pd.concat([self.data, self.error_limitF, self.conditionF], axis=1)
+        self.CSV2.to_csv("graph.csv")
 
-        plt.scatter(x, y, color="red", s=2)
+        z = self.condition.to_numpy()
+
+        colour_condition = np.where(z == False, "k", "red")
+        size_condition = np.where(z == False, 6, 12)
+        plt.scatter(
+            self.Vset, self.percent_error, color=colour_condition, s=size_condition
+        )
 
         plt.plot(
-            x,
-            upper_bound,
-            label="Upper Bound",
-            color="blue",
+            self.Vset,
+            self.error_limit,
+            label="Error Rate (%)",
+            color="red",
             linewidth=1,
         )
         plt.plot(
-            x,
-            lower_bound,
-            label="Lower Bound",
+            self.Vset,
+            self.error_margin,
+            label="Error Margin (%)",
             color="blue",
             linewidth=1,
         )
-        plt.plot(x, a * x + b, label="results", color="red", linewidth=1)
+
+        plt.plot(
+            self.Vset, a * self.Vset + b, label="results", color="green", linewidth=1
+        )
         plt.legend(loc="upper left")
         plt.show()
 
 
-A = datatoGraph()
-A.plotScatter(0.00025, 0.0015)
+# A = datatoGraph(infoList, main.dataList)
+# A.plotScatter(0.00025, 0.0015)
