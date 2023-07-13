d0df8c082aaa6e4b74ac5a8024207d1af3d9412e
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2023-07-13 15:58:48 +0800

    Update PSU.py

--- src/PSU.py
+++ src/PSU.py
@@ -13,35 +13,7 @@ class E36731A(object):
         # Visa Address is found under Keysight Connection Expert
         self.dmm = rm.open_resource(self.VISA_ADDRESS)
         self.dmm.baud_rate = 9600
-
-        # '*IDN?' is standard GPIB Message for "what are you?"
-        self.dmm.timeout = 1000
-        # print(self.dmm.query("*IDN?"))
-
-        # Resets the instrument configuration and synchronizes it before each R/W
         self.dmm.write("*rst")
-        # # self.dmm.write("DISP:TEXT " + '"Reality can be whatever I want"')
-        # self.dmm.write("DISP:TEXT:CLE")
-
-    def apply(self, VOLTAGE_SET, CURRENT_SET):
-        self.setVoltage = VOLTAGE_SET
-        self.setCurrent = CURRENT_SET
-
-        self.dmm.write(
-            "APPL "
-            + self.Channel
-            + ","
-            + str(self.setVoltage)
-            + ","
-            + str(self.setCurrent)
-        )
-
-    def Emulate(self, Emul):
-        self.emul = Emul
-        self.dmm.write("EMUL " + Emul)
-
-    def Output(self, state):
-        self.dmm.write("OUTPUT " + state)
 
     def Voltage_Sweep(self, minVoltage, maxVoltage, Current, Channel, step_size):
         self.Channel = Channel
@@ -58,6 +30,3 @@ class E36731A(object):
         self.Voltage = Voltage
         self.step_size = step_size
         self.iterations = ((maxCurrent - minCurrent) / step_size) + 1
-
-
-# A = E36731A("USB0::0x2A8D::0x5C02::MY62100050::0::INSTR")
