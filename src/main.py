d9a11650674c060e5a7f60152b22240a2fcc7723
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2023-07-17 13:51:47 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,4 @@
-import Data
 import Test
-
-infoList = []
-dataList = []
 
 
 ADDR1 = "GPIB0::5::INSTR"
@@ -11,14 +7,8 @@ ADDR2 = "USB0::0x2A8D::0x5C02::MY62100050::0::INSTR"
 ADDR3 = "USB0::0x2A8D::0x8E01::CN60440004::0::INSTR"
 
 
-A = Test
-A.VisaResourceManager(ADDR1, ADDR2, ADDR3)
+A = Test.VisaResourceManager()
+A.openRM(ADDR1, ADDR2, ADDR3)
 B = Test.VoltageMeasurement(ADDR1, ADDR2, ADDR3, 3, 1)
-infoList, dataList = B.execute()
-
-
-F = Data.instrumentData(ADDR1, ADDR2, ADDR3)
-
-D = Data.datatoCSV(infoList, dataList)
-E = Data.datatoGraph(infoList, dataList)
-E.scatterCompare("Current")
+B.execute()
+A.closeRM()
