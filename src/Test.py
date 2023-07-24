fd27230c6f6927bf71298f1b87a27286ad5d5d79
Zhi Yuan Wong <zhiyuan3112@gmail.com>
2023-07-24 15:59:56 +0800

    Update Test.py
    Added Load Regulation

--- src/Test.py
+++ src/Test.py
@@ -9,7 +9,18 @@ sys.path.insert(
 )
 
 from IEEEStandard import OPC
-from Subsystems import Read, Apply, Display, Function, Output, Sense, Voltage, Current
+from Subsystems import (
+    Read,
+    Apply,
+    Display,
+    Function,
+    Output,
+    Sense,
+    Voltage,
+    Current,
+    Configure,
+)
+from xlreport import xlreport
 
 infoList = []
 dataList = []
@@ -24,7 +35,6 @@ class VisaResourceManager:
         for i in range(len(args)):
             instr = self.rm.open_resource(args[i])
             instr.baud_rate = 9600
-            instr.write("*RST")
             print(instr.query("*IDN?"))
 
     def closeRM(self):
@@ -61,8 +71,8 @@ class VoltageMeasurement:
 
     def execute(self):
         self.settings(0.00025, 0.0015)
-        self.Current_Sweep(1, 2, 0.5)
-        self.Voltage_Sweep(1, 30, 0.5)
+        self.Current_Sweep(1, 2, 1)
+        self.Voltage_Sweep(1, 15, 1)
         i = 0
         j = 0
         k = 0
@@ -105,7 +115,10 @@ class VoltageMeasurement:
         E = Data.datatoGraph(infoList, dataList)
         E.scatterCompare("Voltage", self.param1, self.param2)
 
-    def executeA(
+        G = xlreport()
+        G.run()
+
+    def executeVoltageMeasurement(
         self,
         Error_Gain,
         Error_Offset,
@@ -185,6 +198,7 @@ class CurrentMeasurement:
 
     def settings(self, param1, param2):
         Sense(self.DMM).setCurrentResDC("FAST")
+        # Configure(self.DMM).write("Current")
         Display(self.ELoad).displayState(self.ELoad_Channel)
         Function(self.ELoad).setMode("Voltage", self.ELoad_Channel)
         Voltage(self.PSU).setSenseMode("EXT", 1)
@@ -246,3 +260,136 @@ class CurrentMeasurement:
         D = Data.datatoCSV(infoList, dataList)
         E = Data.datatoGraph(infoList, dataList)
         E.scatterCompare("Current", self.param1, self.param2)
+
+        G = xlreport()
+        G.run()
+
+    def executeCurrentMeasurement(
+        self,
+        Error_Gain,
+        Error_Offset,
+        minCurrent,
+        maxCurrent,
+        current_stepsize,
+        minVoltage,
+        maxVoltage,
+        voltage_stepsize,
+        PSU,
+        DMM,
+        ELoad,
+        ELoad_Channel,
+        PSU_Channel,
+        setVoltage_Sense,
+        setVoltage_Res,
+        setMode,
+    ):
+        dataList = []
+        infoList = []
+        Sense(DMM).setVoltageResDC(setVoltage_Res)
+        Display(ELoad).displayState(ELoad_Channel)
+        Function(ELoad).setMode(setMode, ELoad_Channel)
+        Voltage(PSU).setSenseMode(setVoltage_Sense, PSU_Channel)
+        self.param1 = Error_Gain
+        self.param2 = Error_Offset
+
+        i = 0
+        j = 0
+        k = 0
+        V_fixed = float(minVoltage)
+        V = float(maxVoltage) + 1
+        I = float(minCurrent)
+        current_iter = (
+            (float(maxCurrent) - float(minCurrent)) / float(current_stepsize)
+        ) + 1
+        voltage_iter = (
+            (float(maxVoltage) - float(minVoltage)) / float(voltage_stepsize)
+        ) + 1
+        Output(ELoad).setOutputStateC("ON", ELoad_Channel)
+        Output(PSU).setOutputState("ON")
+
+        while i < voltage_iter:
+            Voltage(ELoad).setOutputVoltage(V_fixed - 0.001 * V_fixed, ELoad_Channel)
+            j = 0
+            I = float(minCurrent)
+            while j < current_iter:
+                Apply(PSU).write(PSU_Channel, V, I)
+                print("Voltage: ", V_fixed, "Current: ", I)
+                infoList.insert(k, [V_fixed, I, i])
+
+                temp_string = float(OPC(PSU).query())
+
+                if temp_string == 1:
+                    dataList.insert(k, [V_fixed, float(Read(DMM).query())])
+                    del temp_string
+
+                I += float(current_stepsize)
+                j += 1
+                k += 1
+
+            V_fixed += float(voltage_stepsize)
+            i += 1
+        Output(PSU).setOutputState("OFF")
+        Output(ELoad).setOutputStateC("OFF", ELoad_Channel)
+        return dataList, infoList
+
+
+class LoadRegulation:
+    def __init__(self, ADDR1, ADDR2, ADDR3, ELoad_Channel, PSU_Channel):
+        self.ELoad = ADDR1
+        self.PSU = ADDR2
+        self.DMM = ADDR3
+        self.ELoad_Channel = ELoad_Channel
+        self.PSU_Channel = PSU_Channel
+
+    def settings(self, param1, param2):
+        Sense(self.DMM).setVoltageResDC("FAST")
+        Display(self.ELoad).displayState(self.ELoad_Channel)
+        Function(self.ELoad).setMode("Current", self.ELoad_Channel)
+        Voltage(self.PSU).setSenseMode("EXT", 1)
+        self.param1 = param1
+        self.param2 = param2
+
+    def specifications(self, V_rating, I_rating, P_rating):
+        self.V_rating = V_rating
+        self.I_rating = I_rating
+        self.P_rating = P_rating
+        self.V_max = self.P_rating / self.I_rating
+
+    def Current_Sweep(self, minCurrent, maxCurrent, step_size):
+        self.minCurrent = minCurrent
+        self.maxCurrent = maxCurrent
+        self.current_step_size = step_size
+        self.current_iter = ((maxCurrent - minCurrent) / step_size) + 1
+
+    def execute(self):
+        self.settings(0.0001, 0.002)
+        self.specifications(5, 20, 200)
+        self.Current_Sweep(0.25, 10, 0.25)
+        iter = 0
+        k = 0
+        I_max = self.maxCurrent + 1
+        V = self.V_rating
+        I_min = self.minCurrent
+        Output(self.PSU).setOutputState("ON")
+        Apply(self.PSU).write(self.PSU_Channel, V, I_max)
+
+        while iter < self.current_iter:
+            Current(self.ELoad).setOutputCurrent(I_min, self.ELoad_Channel)
+            Output(self.ELoad).setOutputStateC("ON", self.ELoad_Channel)
+            temp_string = float(OPC(self.ELoad).query())
+            print("Current : ", I_min, "Load :", I_min * V)
+            infoList.insert(k, [I_min, I_min * V])
+
+            if temp_string == 1:
+                dataList.insert(k, [float(Read(self.DMM).query()), I_min * V])
+                del temp_string
+
+            I_min += self.current_step_size
+            k += 1
+            iter += 1
+
+        Output(self.ELoad).setOutputStateC("OFF", self.ELoad_Channel)
+        Output(self.PSU).setOutputState("OFF")
+        D = Data.datatoCSV_Regulation(
+            infoList, dataList, self.V_rating, self.param1, self.param2
+        )
